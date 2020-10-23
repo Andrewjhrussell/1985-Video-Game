1987551e62310db940868b9ff2e0e8c1f81230ec
Andrewjhrussell <andrewjhrussell@gmail.com>
2020-10-22 21:14:43 -0400

    update

--- static/js/script.js
+++ static/js/script.js
@@ -22,3 +22,36 @@ function reset() {
         div.appendChild(image);
     }
 
+// Challenge 3: Rock, Paper, Scissors
+function rpsGame(yourChoice){
+    console.log(yourChoice); 
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt());
+    console.log('Computer choice:', botChoice);
+    results = decideWinner(humanChoice, botChoice); // [0, 1] human lost | bot won
+    console.log(results);
+   // message = finalMessage(results); // {'message': 'You won!', 'color': 'green'}
+   //rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number){
+    return ['rock','paper', 'scissors'] [number];
+
+}
+
+function decideWinner(yourChoice, computerChoice) {
+   var rpsDatabase = {
+       'rock':{'scissors': 1, 'rock': 0.5, 'paper': 0 },
+       'paper':{'rock': 1, 'paper': 0.5, 'scissors': 0},
+       'scissors':{'paper': 1, 'scissors': 0.5, 'rock': 0}
+   }
+   var yourScore = rpsDatabase[yourChoice][computerChoice];
+   var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+   return[yourScore, computerScore];
+}
